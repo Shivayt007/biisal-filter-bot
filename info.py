777f0fefd65e51433ebce59cd5a580771cf5609a
Race Developer Members <88087547+Shivayt007@users.noreply.github.com>
2024-11-29 08:34:16 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,7 +15,7 @@ def is_enabled(value, default):
 #main variables
 API_ID = int(environ.get('API_ID', '2054877'))
 API_HASH = environ.get('API_HASH', '4227c1e45e462209a3dcc67ada88a44f')
-BOT_TOKEN = environ.get('BOT_TOKEN', "7636083667:AAEdEqzQ1PU69dW6WNaW-JowqJ0F9vm45OU")
+BOT_TOKEN = environ.get('BOT_TOKEN', "7295145755:AAHzuqb-g5v1KE_hW4TaOkNxyyBHSjAgF5U")
 
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7585797432').split()]
 USERNAME = environ.get('USERNAME', "https://telegram.me/TheOften_rids")
